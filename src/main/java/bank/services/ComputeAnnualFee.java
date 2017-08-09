2fa2c2872c7a756db37c2cc227b59804918b4cb3
YifengZhong <yf_zhong@hotmail.com>
2017-08-09 12:45:37 -0500

    visitor pattern

--- src/main/java/bank/services/ComputeAnnualFee.java
+++ src/main/java/bank/services/ComputeAnnualFee.java
@@ -7,17 +7,17 @@ import main.java.bank.domain.SavingAccount;
 /**
  * Created by yf_zh on 2017/08/09.
  */
-public class ComputeAnnualFee implements Compute<CheckingAccount, SavingAccount> {
+public class ComputeAnnualFee implements Compute<SavingAccount,CheckingAccount> {
 
 
 
     @Override
-    public void visitSavingAccount(CheckingAccount checkingAccount) {
+    public void visitCheckAccount(CheckingAccount checkingAccount) {
             checkingAccount.setInterest(0.9);
     }
 
     @Override
-    public void visitCheckAccount(SavingAccount savingAccount) {
+    public void visitSavingAccount(SavingAccount savingAccount) {
             savingAccount.setInterest(0.87);
     }
 }
